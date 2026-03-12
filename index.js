1523aa0462ca65071cfac06228739cfb6bb9386a
Carlos Ramírez Villanueva <carlosramirezvillanueva30@gmail.com>
2026-03-11 20:24:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 
 let { say } = cfonts
 console.log(chalk.magentaBright('\n❀ Iniciando...'))
-say('BOT', {
+say('TOJIBOT-MD', {
 font: 'simple',
 align: 'left',
 gradient: ['green', 'white']
